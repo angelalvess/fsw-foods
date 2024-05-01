2363a0297701d4bcc406903a2025e835098a77cd
angelalvess <angeltrashangel@gmail.com>
2024-05-01 14:14:50 -0300

    feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import CategoryList from './_components/categoryList'
 import Header from './_components/header'
 import { Search } from './_components/search'
+import ProductList from './_components/productList'
+import { Button } from './_components/ui/button'
+import { ChevronRightIcon } from 'lucide-react'
 
 const Home = () => {
   return (
@@ -25,6 +28,21 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="text-1xl font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+
+        <ProductList />
       </div>
     </>
   )
