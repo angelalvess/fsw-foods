d78843541a0c8312c84cedfa793d7f5a40d9e5b4
angelalvess <angeltrashangel@gmail.com>
2024-05-01 02:22:21 -0300

    fix:  input placeholder

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,7 +5,7 @@ import { Input } from './ui/input'
 export const Search = () => {
   return (
     <div className="flex gap-2">
-      <Input placeholder="Search restaurants" className="border-none " />
+      <Input placeholder="Buscar restaurantes" className="border-none " />
       <Button size="icon">
         <SearchIcon size={24} />
       </Button>
