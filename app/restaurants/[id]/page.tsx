c407d5465131c6fb00abd75bd3c881ed911998c7
angelalvess <angeltrashangel@gmail.com>
2024-05-02 12:02:00 -0300

    refactor: restaurant page to include delivery info, restaurant image, and product list

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { db } from '@/app/_lib/prisma'
+import { notFound } from 'next/navigation'
+import RestaurantImage from './_components/restaurantImage'
+import Image from 'next/image'
+import { StarIcon } from 'lucide-react'
+import DeliveryInfo from '@/app/_components/deliveryInfo'
+import ProductList from '@/app/_components/productList'
 
 interface RestaurantPageProps {
   params: {
@@ -6,8 +12,80 @@ interface RestaurantPageProps {
   }
 }
 
-const RestaurantPage = ({ params: { id } }: RestaurantPageProps) => {
-  return <div>{id}</div>
+const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      categories: true,
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  if (!restaurant) {
+    return notFound()
+  }
+
+  return (
+    <div>
+      <RestaurantImage restaurant={restaurant} />
+
+      <div className="flex items-center justify-between px-5 pt-5">
+        <div className="flex items-center gap-[0.375rem]">
+          <div className="relative h-8 w-8">
+            <Image
+              src={restaurant.imageUrl}
+              alt={restaurant.name}
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+
+          <h1 className="text-xl font-bold">{restaurant.name}</h1>
+        </div>
+        <div className="flex items-center gap-[3px] rounded-full bg-foreground px-2 py-[2px] text-white">
+          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+          <span className="text-xs font-semibold">5.0</span>
+        </div>
+      </div>
+
+      <div className="px-5">
+        <DeliveryInfo restaurant={restaurant} />
+      </div>
+
+      <div
+        className="mt-3 flex gap-4 
+      overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden
+      "
+      >
+        {restaurant.categories.map((category) => (
+          <div
+            key={category.id}
+            className="min-w-[167px] rounded-lg bg-[#f4f4f4] py-1 text-center "
+          >
+            <span className="text-xs text-muted-foreground">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-6 space-y-4 ">
+        <h2 className="px-5 font-semibold">Mais pedidos</h2>
+        <ProductList products={restaurant.products} />
+      </div>
+    </div>
+  )
 }
 
 export default RestaurantPage
