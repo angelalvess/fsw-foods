0096fe60df5c678bfaf38d87bbf2f92aae8d4d24
angelalvess <angeltrashangel@gmail.com>
2024-05-02 12:01:17 -0300

    refactor: product details component to use delivery info component

--- app/products/[id]/_components/productDetails.tsx
+++ app/products/[id]/_components/productDetails.tsx
@@ -8,15 +8,10 @@ import { Prisma } from '@prisma/client'
 import Image from 'next/image'
 import DiscountBadge from '@/app/_components/discountBadge'
 import { Button } from '@/app/_components/ui/button'
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from 'lucide-react'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 import { useState } from 'react'
-import { Card } from '@/app/_components/ui/card'
 import ProductList from '@/app/_components/productList'
+import DeliveryInfo from '@/app/_components/deliveryInfo'
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -104,36 +99,11 @@ const ProductDetails = ({
       </div>
 
       <div className="px-5">
-        <Card className="items-cente mt-6 flex justify-around py-3 ">
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground ">
-              <span className="text-xs">Entrega</span>
-              <BikeIcon size={14} />
-            </div>
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <span className="text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </span>
-            ) : (
-              <span className="text-xs font-semibold">Grátis</span>
-            )}
-          </div>
-
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground ">
-              <span className="text-xs">Entrega</span>
-              <TimerIcon size={14} />
-            </div>
-
-            <span className="text-xs font-semibold">
-              {product.restaurant.deliveryTimeMinutes} min
-            </span>
-          </div>
-        </Card>
+        <DeliveryInfo restaurant={product.restaurant} />
       </div>
 
       <div className="mt-6 space-y-3 px-5">
-        <h3 className=" font-semibold">Sobre</h3>
+        <h3 className="font-semibold">Sobre</h3>
         <p className="text-sm text-muted-foreground">{product.description}</p>
       </div>
 
