06acbbd4bf8c229e6f2940097dc06095a26247d9
angelalvess <angeltrashangel@gmail.com>
2024-05-01 22:55:02 -0300

    feat: add product page

---
+++ app/products/[id]/_components/productImage.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Product } from '@prisma/client'
+import { Button } from '@/app/_components/ui/button'
+import { ChevronLeftIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+
+interface ProductImageProps {
+  product: Pick<Product, 'imageUrl' | 'name'>
+}
+
+const ProductImage = ({ product }: ProductImageProps) => {
+  const router = useRouter()
+
+  const handleBackClick = () => {
+    router.back()
+  }
+
+  return (
+    <div className="relative h-[360px] w-full">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-cover"
+      />
+
+      <Button
+        onClick={handleBackClick}
+        className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
+        size="icon"
+      >
+        <ChevronLeftIcon />
+      </Button>
+    </div>
+  )
+}
+
+export default ProductImage
