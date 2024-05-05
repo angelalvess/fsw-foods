a552a8e20485723736d943e076d58558842b933b
angelalvess <angeltrashangel@gmail.com>
2024-05-05 19:12:44 -0300

    fix:husky

--- app/restaurants/[id]/_components/cartBanner.tsx
+++ app/restaurants/[id]/_components/cartBanner.tsx
@@ -1,39 +1,39 @@
-"use client";
-// aa
+'use client'
+// a
 
-import Cart from "@/app/_components/cart";
-import { Button } from "@/app/_components/ui/button";
+import Cart from '@/app/_components/cart'
+import { Button } from '@/app/_components/ui/button'
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
-} from "@/app/_components/ui/sheet";
-import { CartContext } from "@/app/_context/cart";
-import { formatCurrency } from "@/app/_helpers/price";
-import { Restaurant } from "@prisma/client";
-import { useContext, useState } from "react";
+} from '@/app/_components/ui/sheet'
+import { CartContext } from '@/app/_context/cart'
+import { formatCurrency } from '@/app/_helpers/price'
+import { Restaurant } from '@prisma/client'
+import { useContext, useState } from 'react'
 
 interface CartBannerProps {
-  restaurant: Pick<Restaurant, "id">;
+  restaurant: Pick<Restaurant, 'id'>
 }
 
 const CartBanner = ({ restaurant }: CartBannerProps) => {
-  const { products } = useContext(CartContext);
-  const { calculateTotalPrice } = useContext(CartContext);
-  const { calculateTotalQuantity } = useContext(CartContext);
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const { products } = useContext(CartContext)
+  const { calculateTotalPrice } = useContext(CartContext)
+  const { calculateTotalQuantity } = useContext(CartContext)
+  const [isCartOpen, setIsCartOpen] = useState(false)
 
   const handleIsCartOpen = () => {
-    setIsCartOpen(!isCartOpen);
-  };
+    setIsCartOpen(!isCartOpen)
+  }
 
   const restaurantHasProductsOnCart = products.some(
     (product) => product.restaurantId === restaurant.id,
-  );
+  )
 
   if (!restaurantHasProductsOnCart) {
-    return null;
+    return null
   }
   return (
     <div className="fixed bottom-0 left-0 z-50 w-full border-t border-solid border-muted bg-white p-5 pt-3 shadow-md">
@@ -43,10 +43,10 @@ const CartBanner = ({ restaurant }: CartBannerProps) => {
             Total sem entrega
           </span>
           <h3 className="font-semibold">
-            {formatCurrency(calculateTotalPrice)}{" "}
+            {formatCurrency(calculateTotalPrice)}{' '}
             <span className="text-xs font-normal text-muted-foreground">
-              / {calculateTotalQuantity}{" "}
-              {calculateTotalQuantity > 1 ? "itens" : "item"}
+              / {calculateTotalQuantity}{' '}
+              {calculateTotalQuantity > 1 ? 'itens' : 'item'}
             </span>
           </h3>
         </div>
@@ -63,7 +63,7 @@ const CartBanner = ({ restaurant }: CartBannerProps) => {
         </Sheet>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CartBanner;
+export default CartBanner
